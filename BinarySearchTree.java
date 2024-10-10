8f040775c3658a0da657d0148fc02ce9c9560d53
Skinken <guma.moritz@gmail.com>
2024-10-10 11:31:16 +0200

    comments done for task 1

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,4 +1,10 @@
+/*
+ * BinarySearchTree class 
+ */
 public class BinarySearchTree<Key extends Comparable<Key>, Value>{
+    /*
+     * The node class used for the BinarySearchTree
+     */
     private class Node{
         Value value;
         Key key;
@@ -12,6 +18,14 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value>{
             this.left = null;
         }
 
+        /*
+         * Recursive add method on a node 
+         * Recursivly class the add method one either right or left child of the root node until the correct location for the new node is found
+         * If the node to be added has the same Key key as another node already in the tree the value at that node is replaced with the value of the new node
+         * 
+         * @param key, the key of the new node, needs to be a comparable datatype
+         * @param val, the value stored in the new node, could be any form of data such as a object or a simple string
+         */
         private void add(Key key, Value val){
             if (this.key.equals(key)){
                 this.value = val;
@@ -27,6 +41,14 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value>{
             }
         }
 
+        /*
+         * Recursive search method which looks through the tree and returns true or false
+         * depending on wether the serached for key is present in the tree.
+         * Since this is a BinarySearchTree the nodes of the tree are orderd by size from left to right
+         * we use this to serach left or right depending on the size of the key.
+         * 
+         * @param key, the key of the node we are trying to find in the tree
+         */
         private boolean lookup(Key key){
             if (this.key.equals(key)) return true;
             else if(this.key.compareTo(key) > 0){
@@ -39,29 +61,54 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value>{
             }
         }
 
+        /*
+         * Print method using the programming language implicit stack to recusivly move through the tree and print out its values in order
+         */
         private void printImplicitStack() {
             if(this.left != null) this.left.printImplicitStack();
             System.out.println(this.key + " " + this.value);
             if(this.right != null) this.right.printImplicitStack();
         }
     }
-
+/*
+ * The beginning of the BinarySearchTree class
+ */
     private Node root;
 
     public BinarySearchTree(){
         this.root = null;
     }
 
+    /*
+     * Add operation to add a new node to the tree
+     * Creates a new root if the tree is empty otherwise we call the add method in the the node class on the root
+     * 
+     * @param key, the key of the new node
+     * @param val, the value stored in the new node
+     */
     public void add(Key key, Value val){
         if (root == null) root = new Node(key, val);
         else root.add(key, val);
     }
 
+    /*
+     * A search method to find out wether a node with a certain key is present in the tree
+     * 
+     * @param key, the key we are searching for
+     */
     public boolean lookup(Key key){
         if (root == null) return false;
         else return root.lookup(key);
     }
 
+    /*
+     * A add operation without using recursion to move through the tree
+     * In this implementation we instead keep track of where we are in the tree when finding the correct place for the new node
+     * Uses the BinarySearchTree orderd structure to at every node go down left or right until a fitting empty spot is located
+     * 
+     * @param key, the key of the new node
+     * @param val, the value stored in the new node
+     */
     public void addWithoutRecursion(Key key, Value val){
         if (root == null) {
             root = new Node(key, val);
@@ -90,14 +137,23 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value>{
         }
     }
 
+    /*
+     * Prints the entire tree connected to the root node in order
+     */
     public void printTreeImplicitStack(){
         root.printImplicitStack();
     }
 
+    /*
+     * Prints the entire tree connected to the root node in order 
+     * This method uses a self implemented explicit stack instead of using recursion on the implicit programming stack
+     * 
+     */
     public void printExplicitStack() {
         Dynamic<Node> nodeStack = new Dynamic<Node>();
         Node current = this.root;
-        // move to the leftmost node
+        // move to the leftmost node pushing nodes on the stack
+        //we push these nodes in order to be able to go back up the tree
         while(current.left != null){
             nodeStack.push(current);
             current = current.left;
@@ -115,6 +171,8 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value>{
                 }
             } 
             else{
+                //While at the leftmost node we print that node and pop the stack
+                //This means that we set current to the first element on the stack enabling us to move back up the tree
                 current = nodeStack.pop();
             }
         }
