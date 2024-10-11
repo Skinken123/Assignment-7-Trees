b6c2690a8f1ea7d6ef23eb5b67267716969a9347
Skinken <guma.moritz@gmail.com>
2024-10-11 10:30:27 +0200

    created benchamrks for add and lookup methods

--- BenchmarkTree.java
+++ BenchmarkTree.java
@@ -1,5 +1,8 @@
+import java.util.*;
+
 public class BenchmarkTree{
     public static void main(String[] args){
+        /* 
         BinarySearchTree<Integer, String> newTree = new BinarySearchTree<Integer, String>();
         newTree.add(7, "Sju");
         newTree.add(10, "Tio");
@@ -38,5 +41,82 @@ public class BenchmarkTree{
         newTreeSequence.nextValue();
         newTreeSequence.nextValue();
         newTreeSequence.nextValue();
+        */
+        //benchmarkAdd();
+        benchmarkLookup();
+    }
+
+    public static BinarySearchTree<Integer, Integer> generateTree(int size){
+        Random random = new Random();
+        BinarySearchTree<Integer, Integer> bst = new BinarySearchTree<>();
+        bst.add(5000, 5000);
+
+        // Insert `size` random elements into the tree
+        for (int i = 0; i < size - 1; i++) {
+            int key = random.nextInt(10000);  // Generate random key
+            bst.add(key, key);                // Insert key into the tree
+        }
+        return bst;
+    }
+
+    public static int[] randomNumbers(){
+        Random random = new Random();
+        int[] numbers = new int[10];
+        for(int i = 0; i<10;i++){
+            numbers[i] = random.nextInt(10000);
+        }
+        return numbers;
+    }
+
+    public static void benchmarkAdd(){
+        int[] sizes = {100, 200, 400, 800, 1600, 3200};
+        int k = 5000;
+        for (int s : sizes){
+            long min = Long.MAX_VALUE;
+            long total = 0;
+            for (int i = 0; i < k; i++){
+                long temp = runAdd(s);
+                if (temp < min) min = temp;
+                total += temp;
+            }
+            System.out.print(s + " " + min + " " + total/k + "\n");
+        }
+    }
+
+    public static long runAdd(int size){
+        BinarySearchTree<Integer, Integer> testTree = generateTree(size);
+        int[] numbers = randomNumbers();
+        long n1 = System.nanoTime();
+        for(int n : numbers){
+            testTree.add(n, n);
+        }
+        long n2 = System.nanoTime();
+        return (n2-n1)/10;
+    }
+
+    public static void benchmarkLookup(){
+        int[] sizes = {100, 200, 400, 800, 1600, 3200};
+        int k = 5000;
+        for (int s : sizes){
+            long min = Long.MAX_VALUE;
+            long total = 0;
+            for (int i = 0; i < k; i++){
+                long temp = runLookup(s);
+                if (temp < min) min = temp;
+                total += temp;
+            }
+            System.out.print(s + " " + min + " " + total/k + "\n");
+        }
+    }
+
+    public static long runLookup(int size){
+        BinarySearchTree<Integer, Integer> testTree = generateTree(size);
+        int[] numbers = randomNumbers();
+        long n1 = System.nanoTime();
+        for(int n : numbers){
+            testTree.lookup(n);
+        }
+        long n2 = System.nanoTime();
+        return (n2-n1)/10;
     }
 }
